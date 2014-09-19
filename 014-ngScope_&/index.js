5acbdeb3ca16b6d49e999b7d3245de68b2099200
Jared Halpert <jaredhalpert90@gmail.com>
2014-09-19 12:39:26 -0700

    added: isolate scope w/ & (any expression can be used) example

---
+++ 014-ngScope_&/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var app = angular.module('app', []);
+
+
+app.controller('AppCtrl', function ($scope) {
+  $scope.callHome = function (message) {
+    alert(message);
+  };
+});
+
+
+// the dial directive needs to be independent from the phone directive
+// therefore, it's isolated w/in each phone directive instance
+// the model is bound to the controller (how data is communicated)
+app.directive('phone', function () {
+  return {
+    scope: {
+      dial: '&'
+    },
+    template: '<input type="text" ng-model="value"/>' +
+              '<div class="button" ng-click="dial({ message: value })">' +
+              'Call Home' +
+              '</div>'
+  };
+});
